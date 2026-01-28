8b4bf5ef49af8e47c4e3d8977eaea5b3848ac293
João Sereia <joao.lo.sereia@gmail.com>
2026-01-28 05:04:49 -0300

    feat(entities/enums): add enum with supported audio file extensions

---
+++ lib/src/domain/enums/file_extension_enum.dart
@@ -0,0 +1,20 @@
+/// Enum of supported file extensions.
+enum FileExtensionEnum {
+  /// .mp3
+  mp3,
+
+  /// .flac
+  flac,
+
+  /// .mp4
+  mp4,
+
+  /// .m4a
+  m4a,
+
+  /// .m4v
+  m4v,
+
+  /// .m4b
+  m4b,
+}
