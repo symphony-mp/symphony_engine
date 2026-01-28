a8ad132523d5ff6a3758ce49113da307834916f9
João Sereia <joao.lo.sereia@gmail.com>
2026-01-28 05:15:57 -0300

    feat(lib): add package entry point and the library initialization class

--- lib/symphony_engine.dart
+++ lib/symphony_engine.dart
@@ -1,3 +1,28 @@
-/// An open source music player that contains the main local and offline
-/// features.
+/// An open source music player that contains the main
+/// local and offline features.
 library;
+
+import 'dart:developer';
+
+import 'package:soft_metatags/soft_metatags.dart';
+
+export 'entities.dart';
+export 'repositories.dart';
+export 'services.dart';
+
+/// [SymphonyEngine] library;
+final class SymphonyEngine {
+  const SymphonyEngine._();
+
+  /// Initializes the [SymphonyEngine] library.
+  static Future<void> initialize() async {
+    await SoftMetaTags.initialize();
+    log('Initialized!', name: 'SymphonyEngine');
+  }
+
+  /// Disposes the [SymphonyEngine] library.
+  static Future<void> dispose() async {
+    SoftMetaTags.dispose();
+    log('Disposed!', name: 'SymphonyEngine');
+  }
+}
